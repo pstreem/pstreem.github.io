9305b07597022d71628928f3af6fa030ad3b1636
pstreem <pstreem@gmail.com>
2020-09-08 16:59:56 +0800

    Tue Sep  8 16:59:56 2020

--- pushcontent.py
+++ pushcontent.py
@@ -7,13 +7,10 @@ import time
 #repo = git.Repo.init(path='/home/pstreem/project/github')
 repo = git.Repo('/home/pstreem/project/github')
 
+print (repo.git.pull())
 print (repo.git.add('.'))
 
-print (repo.git.pull())
-
 date = time.ctime()
-commit_comment = '-m \"'+date+'\"'
-#print (repo.git.commit(commit_comment))
 print (repo.git.commit('-m', date))
 
 print (repo.git.push())
